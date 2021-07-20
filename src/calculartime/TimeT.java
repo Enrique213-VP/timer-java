0e2576c97a2511bbf899b7296ffc9e0a251c2d18
Sergio <colombia00028@gmail.com>
2021-07-19 19:17:01 -0500

    proyecto timer

---
+++ src/calculartime/TimeT.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculartime;
+
+import javax.swing.JOptionPane;
+
+
+public class TimeT {
+    
+    
+    private Integer dia = 0;
+
+    
+    public TimeT(){
+        
+    }
+
+    public void getDia(String a){
+        dia = Integer.parseInt(a);
+    }
+    
+    
+}
+
