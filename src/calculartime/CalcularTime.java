f68e1fe45bc9d8cfcba1c8392ad029491960be14
Sergio <colombia00028@gmail.com>
2021-07-19 19:09:20 -0500

    Design de contador en applet

---
+++ src/calculartime/CalcularTime.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculartime;
+
+/**
+ *
+ * @author sevar
+ */
+public class CalcularTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Time prueba = new Time();
+       
+       prueba.setLocationRelativeTo(null);
+       prueba.setTitle("Hora minutos");
+       prueba.setVisible(true);
+    }
+    
+}
